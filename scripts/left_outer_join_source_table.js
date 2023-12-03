8ca4823abe2ccd51a144829386ac292a30966786
Khanh Nam <namdk@gmail.com>
2023-12-03 23:24:33 +1100

    Finished left outer join

--- scripts/left_outer_join_source_table.js
+++ scripts/left_outer_join_source_table.js
@@ -10,48 +10,64 @@ class LeftOuterJoinSourceTable extends SourceTable {
     super.updateTargetRow();
 
     // Fill in the html values for the target table's row
-    for (let i=this.startColumn; i<this.startColumn + this.data[0].length; ++i) {
+    for (let i = this.startColumn; i < this.startColumn + this.data[0].length; ++i) {
       this.targetRow.cells[i].innerHTML = this.draggingElement.cells[i - this.startColumn].innerHTML;
     }
 
     const compareCell1 = this.targetRow.cells[this.compareColumns[0]].innerHTML;
     const compareCell2 = this.targetRow.cells[this.compareColumns[1]].innerHTML;
     if (compareCell1 === "-" || compareCell2 == "-") {
-        this.targetRow.classList.remove("incorrect");
-        this.targetRow.classList.remove("correct");
+      this.targetRow.classList.remove("incorrect");
+      this.targetRow.classList.remove("correct");
     }
-    else {
-      if(!this.keyExistInOtherTable(compareCell1) && compareCell2 === "-")
-      {
-        SourceTable.setRowCorrect(this.targetRow, true);
-  
-        this.errorList.removeError(this.targetRowID);
-      }
-      else if (compareCell1 === compareCell2) {
-        SourceTable.setRowCorrect(this.targetRow, true);
+    // If there is no match in the right source table, set row to true since it's left outer join
+    if (!this.keyExistInOtherTable(compareCell1) && compareCell2 == "-" && this.sourceIndex == 0) {
+      SourceTable.setRowCorrect(this.targetRow, true);
+      this.errorList.removeError(this.targetRowID);
+    }
+    // If there is no match in the left source table, set row to false
+    else if (!this.keyExistInOtherTable(compareCell2) && this.sourceIndex == 1) {
+      SourceTable.setRowCorrect(this.targetRow, false);
 
+      this.errorList.addError(
+        this.targetRowID,
+        "Error on row " + this.targetRowID + ": Employee.DepartmentID != Department.DepartmentID or there is no matching row from the other source table",
+        "Find a row in Authors table with ID " + compareCell1
+      );
+    }
+    // Standard matching id check 
+    else {
+      if (compareCell1 === compareCell2) {
+        SourceTable.setRowCorrect(this.targetRow, true);
         this.errorList.removeError(this.targetRowID);
       }
-      else {
+      else if (this.sourceIndex == 0 && compareCell2 != "-" || this.sourceIndex == 1 && compareCell1 != "-") {
         SourceTable.setRowCorrect(this.targetRow, false);
 
         this.errorList.addError(
           this.targetRowID,
-          "Error on row " + this.targetRowID + ": Employee.EmployeeID != Department.DepartmentID or there is no matching row from the other source table" ,
+          "Error on row " + this.targetRowID + ": Employee.DepartmentID != Department.DepartmentID or there is no matching row from the other source table",
           "Find a row in Authors table with ID " + compareCell1
         );
       }
-    }    
-  }
-  keyExistInOtherTable(key)
-  {
-    console.log(this.manager.sourceTable[1].table.rowCount);
-    for(let i = 0; i < this.manager.sourceTable[1].table.rowCount; i++)
-    {
-      console.log(this.manager.sourceTable[1].data[i][0])
-      if(key === this.manager.sourceTable[1].data[i][0])
-        return true
     }
-    return false
+  }
+  keyExistInOtherTable(key) {
+    if (this.sourceIndex === 0) {
+      for (let i = 1; i < this.manager.sourceTables[1].data.length; i++) {
+        if (key == this.manager.sourceTables[1].data[i][0]) {
+          return true
+        }
+      }
+      return false
+    }
+    else {
+      for (let i = 1; i < this.manager.sourceTables[0].data.length; i++) {
+        if (key == this.manager.sourceTables[0].data[i][2]) {
+          return true
+        }
+      }
+      return false
+    }
   }
 }
